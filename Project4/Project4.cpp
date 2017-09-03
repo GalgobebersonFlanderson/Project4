d6bc2b8fe5a04f89babf347c08eea6754c332e91
Kirill Zhdakh <kzhdakh@fullsail.edu>
2017-09-03 00:32:45 -0400

    InitialRepo

---
+++ Project4/Project4.cpp
@@ -0,0 +1,273 @@
+// Project4.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "Project4.h"
+#include "d3d11.h"
+#include <DirectXColors.h>
+
+#pragma comment(lib, "d3d11.lib")
+
+#define MAX_LOADSTRING 100
+#define BufferWidth 1000
+#define BufferHeight 950
+
+// Global DirectX Variables
+ID3D11Device*				m_pDevice = nullptr;
+ID3D11DeviceContext*		m_pContext = nullptr;
+ID3D11RenderTargetView*		m_pRtv = nullptr;
+IDXGISwapChain*				m_pSwapChain = nullptr;
+D3D_DRIVER_TYPE				m_DriverType;
+D3D_FEATURE_LEVEL			m_FeatureLevel;
+D3D11_VIEWPORT				m_ViewPort;
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int, HWND&);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_PROJECT4, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+	HWND window;
+    if (!InitInstance (hInstance, nCmdShow, window))
+    {
+        return FALSE;
+    }
+
+	// Initialize DirectX11
+	D3D_DRIVER_TYPE driverTypes[] =
+	{
+		D3D_DRIVER_TYPE_HARDWARE,
+		D3D_DRIVER_TYPE_WARP,
+		D3D_DRIVER_TYPE_REFERENCE
+	};
+	UINT driverCount = ARRAYSIZE(driverTypes);
+
+	D3D_FEATURE_LEVEL featureLevels[] = 
+	{
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+		D3D_FEATURE_LEVEL_9_3,
+		D3D_FEATURE_LEVEL_9_2,
+		D3D_FEATURE_LEVEL_9_1
+	};
+	UINT featureCount = ARRAYSIZE(featureLevels);
+
+	DXGI_SWAP_CHAIN_DESC swapDesc;
+	ZeroMemory(&swapDesc, sizeof(DXGI_SWAP_CHAIN_DESC));
+	swapDesc.BufferCount = 1;
+	swapDesc.BufferDesc.Width = BufferWidth;
+	swapDesc.BufferDesc.Height = BufferHeight;
+	swapDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapDesc.BufferDesc.RefreshRate.Numerator = 60;
+	swapDesc.BufferDesc.RefreshRate.Denominator = 1;
+	swapDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	swapDesc.Windowed = true;
+	swapDesc.SampleDesc.Count = 1;
+	swapDesc.SampleDesc.Quality = 0;
+	swapDesc.Flags = NULL;
+	swapDesc.OutputWindow = window;
+
+	HRESULT result;
+	for (unsigned int i = 0; i < driverCount; ++i)
+	{
+		result = D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, NULL,
+			featureLevels, featureCount, D3D11_SDK_VERSION, &swapDesc, &m_pSwapChain, &m_pDevice,
+			&m_FeatureLevel, &m_pContext);
+		if (SUCCEEDED(result))
+		{
+			m_DriverType = driverTypes[i];
+			break;
+		}
+	}
+
+	// Create RTV
+	ID3D11Texture2D*	m_pBackBufferTexture = 0;
+	m_pSwapChain->GetBuffer(NULL, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(&m_pBackBufferTexture));
+	m_pDevice->CreateRenderTargetView(m_pBackBufferTexture, nullptr, &m_pRtv);
+
+	// Binding RTV
+	m_pContext->OMSetRenderTargets(1, &m_pRtv, nullptr);
+
+	// Create Viewport
+	m_ViewPort.Width = static_cast<float>(BufferWidth);
+	m_ViewPort.Height = static_cast<float>(BufferHeight);
+	m_ViewPort.TopLeftX = 0;
+	m_ViewPort.TopLeftY = 0;
+	m_ViewPort.MinDepth = 0.0f;
+	m_ViewPort.MaxDepth = 1.0f;
+
+	// Binding Viewport
+	m_pContext->RSSetViewports(1, &m_ViewPort);
+
+	// Clearing RTV
+	m_pContext->ClearRenderTargetView(m_pRtv, DirectX::Colors::White);
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_PROJECT4));
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+		m_pSwapChain->Present(0, 0);
+    }
+
+	// Shutdown DirectX11
+	if (m_pBackBufferTexture) m_pBackBufferTexture->Release();
+	if (m_pContext) m_pContext->Release();
+	if (m_pDevice) m_pDevice->Release();
+	if (m_pRtv) m_pRtv->Release();
+	if (m_pSwapChain) m_pSwapChain->Release();
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_PROJECT4));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_PROJECT4);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow, HWND& h)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   h = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, BufferWidth, BufferHeight, nullptr, nullptr, hInstance, nullptr);
+
+   if (!h)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(h, nCmdShow);
+   UpdateWindow(h);
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Parse the menu selections:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Add any drawing code that uses hdc here...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
